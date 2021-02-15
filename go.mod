a9b3dd031ab71b4a541bedc3d0a849ace3cb48aa
jack bao <1935009637@qq.com>
2021-02-15 17:16:56 +0800

    zip

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
 	github.com/micro/go-micro/v2 v2.9.1
 	github.com/micro/go-plugins/config/source/consul/v2 v2.9.1
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/uber/jaeger-client-go v2.25.0+incompatible
 	github.com/uber/jaeger-lib v2.4.0+incompatible // indirect
+	go.uber.org/zap v1.16.0 // indirect
 )
---
+++ zap.go
@@ -0,0 +1,94 @@
+package common
+
+import (
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	logger *zap.SugaredLogger
+)
+
+func init() {
+	fileName := "micro.log"
+	syncWriter := zapcore.AddSync(
+		&lumberjack.Logger{
+			Filename:   fileName,
+			MaxSize:    512,
+			MaxAge:     30,
+			MaxBackups: 1,
+			LocalTime:  true,
+			Compress:   true,
+		})
+	//编码
+	encoder := zap.NewProductionEncoderConfig()
+	//时间格式
+	encoder.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(
+		//编码器
+		zapcore.NewJSONEncoder(encoder),
+		syncWriter,
+		zap.NewAtomicLevelAt(zap.DebugLevel))
+	log := zap.New(
+		core,
+		zap.AddCaller(),
+		zap.AddCallerSkip(1))
+	logger = log.Sugar()
+}
+
+func Debug(args ...interface{}) {
+	logger.Debug(args)
+}
+
+func Debugf(template string, args ...interface{}) {
+	logger.Debugf(template, args...)
+}
+
+func Info(args ...interface{}) {
+	logger.Info(args...)
+}
+
+func Infof(template string, args ...interface{}) {
+	logger.Infof(template, args...)
+}
+
+func Warn(args ...interface{}) {
+	logger.Warn(args...)
+}
+
+func Warnf(template string, args ...interface{}) {
+	logger.Warnf(template, args...)
+}
+
+func Error(args ...interface{}) {
+	logger.Error(args...)
+}
+
+func Errorf(template string, args ...interface{}) {
+	logger.Errorf(template, args...)
+}
+
+func DPanic(args ...interface{}) {
+	logger.DPanic(args...)
+}
+
+func DPanicf(template string, args ...interface{}) {
+	logger.DPanicf(template, args...)
+}
+
+func Panic(args ...interface{}) {
+	logger.Panic(args...)
+}
+
+func Panicf(template string, args ...interface{}) {
+	logger.Panicf(template, args...)
+}
+
+func Fatal(args ...interface{}) {
+	logger.Fatal(args...)
+}
+
+func Fatalf(template string, args ...interface{}) {
+	logger.Fatalf(template, args...)
+}
